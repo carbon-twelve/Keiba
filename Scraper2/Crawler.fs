2fc57afca622a5ba3b0d32fde1c6a49210b9153f
carbon_twelve <carbon.tetrachloride@gmail.com>
2016-03-01 00:52:02 +0900

    KB-1: エラー時の処理を追加

--- Scraper2/Crawler.fs
+++ Scraper2/Crawler.fs
@@ -17,10 +17,18 @@ let log = TraceSource("Log")
 let rootDir: string = @"C:\Users\Takayuki\Projects\Keiba"
 let baseUrl: string = "http://db.netkeiba.com"
 
-let sleep () = Thread.Sleep(5000)
+let sleep () = Thread.Sleep(15000)
 let client = new HttpClient()
 let crawlSemaphore = new Semaphore(1, 1)
-let monthlySemaphore = new Semaphore(1, 1)
+
+let rec retry (count: int) (async: Async<'T>): 'T =
+    if count > 3 then failwith "Exausted retries" else
+    let result =
+        try Some(Async.RunSynchronously async)
+        with _ -> None
+    match result with
+        | Some(x) -> x
+        | None -> Thread.Sleep(600 * 1000); retry (count + 1) async
 
 let loadHtmlAsStringAsync (uri: Uri): Async<string> =
     async {
@@ -53,7 +61,7 @@ let monthlyPages (startDate: LocalDate) (endDate: LocalDate): HtmlDocument seq =
     seq {
         for currentMonth in dateRangeMonthly startDate endDate do
             let currentMonthPath = "/?pid=race_top&date=" + currentMonth.ToString("yyyyMMdd", null)
-            yield loadHtmlAsync (new Uri(baseUrl + currentMonthPath)) |> Async.RunSynchronously
+            yield loadHtmlAsync (new Uri(baseUrl + currentMonthPath)) |> retry 0
     }
 
 let raceListRegex = Regex("/race/list/(\d+)")
@@ -63,25 +71,27 @@ let raceListPages (startDate: LocalDate) (endDate: LocalDate) (monthlyPage: Html
     seq {
         for node in monthlyPage.DocumentNode.SelectNodes("//table[@summary=\"レーススケジュールカレンダー\"]//a") do
             let raceListPath = node.Attributes.["href"].Value
-            let raceDate =
-                let result = localDatePattern.Parse(raceListRegex.Match(raceListPath).Groups.[1].Value)
-                result.GetValueOrThrow()
-            if startDate <= raceDate && raceDate <= endDate then
-                yield loadHtmlAsync (new Uri(baseUrl + raceListPath)) |> Async.RunSynchronously
+            if raceListRegex.IsMatch(raceListPath) then
+                let raceDate =
+                    let result = localDatePattern.Parse(raceListRegex.Match(raceListPath).Groups.[1].Value)
+                    result.GetValueOrThrow()
+                if startDate <= raceDate && raceDate <= endDate then
+                    yield loadHtmlAsync (new Uri(baseUrl + raceListPath)) |> retry 0
     }
 
 let raceNumberRegex = Regex("/race/(\d+)/")
 
-let racePages (raceListPage: HtmlDocument): (HtmlDocument * string) seq =
+let racePages (raceListPage: HtmlDocument): (string * string) seq =
     seq {
         for node in raceListPage.DocumentNode.SelectNodes("//dl[@class=\"race_top_data_info fc\"]/dd/a") do
             let racePath = node.Attributes.["href"].Value
-            let raceNumber = raceNumberRegex.Match(racePath).Groups.[1].Value
-            let html = loadHtmlAsync (new Uri(baseUrl + racePath)) |> Async.RunSynchronously
-            yield (html, raceNumber)
+            if raceNumberRegex.IsMatch(racePath) then
+                let raceNumber = raceNumberRegex.Match(racePath).Groups.[1].Value
+                let html = loadHtmlAsStringAsync (new Uri(baseUrl + racePath)) |> retry 0
+                yield (html, raceNumber)
     }
 
-let saveRacePageAsync (racePage: HtmlDocument, raceNumber: string): Async<unit> =
+let saveRacePageAsync (racePage: string, raceNumber: string): Async<unit> =
     async {
         let filePath = rootDir + @"\race\" + raceNumber + ".html"
         use writer = new StreamWriter(File.Create(filePath), Encoding.GetEncoding("EUC-JP"))
@@ -94,15 +104,13 @@ let saveRacePageAsync (racePage: HtmlDocument, raceNumber: string): Async<unit> 
 let crawl (startDate: LocalDate) (endDate: LocalDate): unit =
     for monthlyPage in monthlyPages startDate endDate do
         for raceListPage in raceListPages startDate endDate monthlyPage do
-            Seq.map saveRacePageAsync (racePages raceListPage)
-            |> Async.Parallel
-            |> Async.RunSynchronously
-            |> ignore
-
+            for racePage in racePages raceListPage do
+                Async.Start (saveRacePageAsync racePage)
+            
 [<EntryPoint>]
 let main argv = 
     use consoleListener = new ConsoleTraceListener()
     log.Listeners.Add(consoleListener) |> ignore
     log.Switch.Level <- SourceLevels.Verbose
-    crawl (LocalDate(1986,8,1)) (LocalDate(2016,2,28)) |> ignore
+    crawl (LocalDate(1987,11,1)) (LocalDate(2016,2,28)) |> ignore
     0 // return an integer exit code
